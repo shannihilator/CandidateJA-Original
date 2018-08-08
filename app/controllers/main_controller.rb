f4af4bfb7ccb725a597e6fcd7c1260486a492dcb
anonymous <anonymous>
2018-08-08 13:31:47 -0700

    Took Postman script to retrieve API data

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,20 @@
 class MainController < ApplicationController
   def index
-    render text: "", layout: "application"
+    require 'uri'
+    require 'net/http'
+    require 'json'
+
+    url = URI("https://api.salesloft.com/v2/people.json")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = 'Bearer ' + ENV["API_KEY"]
+    request["Cache-Control"] = 'no-cache'
+
+
+    response = http.request(request)
+    @body = response.read_body
   end
 end
